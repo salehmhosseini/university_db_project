56b79fe27ba45b3751dcfa247a9cd255798f294f
Saleh.mh <salehmhosseini2002@gmail.com>
2023-06-21 19:42:44 +0330

    add requirements.txt file and change the directories order

--- src/temp.py
+++ src/temp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('login_page.html')
+    return render_template('login_page_pooya.html')
 
 if __name__ == '__main__':
     app.run()
